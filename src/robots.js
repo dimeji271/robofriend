bda399b4990ab123274f0fb88ff995f5093b10ba
Fasasi Olayemi <fashyemitte@gmail.com>
2022-03-16 00:01:50 +0100

    Added ErrorBoundry

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Hassan Hakeem',
+        username: 'admiral',
+        email: 'admiral@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Olaitan Hammed',
+        username: 'bobollus',
+        email: 'hammed@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Fasasi Olayemi',
+        username: 'fasasi',
+        email: 'fasasi@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Hassan Folawiwo',
+        username: 'hammed',
+        email: 'hammed@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Annie Dorcas',
+        username: 'annie',
+        email: 'annie@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Hammed Folawiwo',
+        username: 'hammedfola',
+        email: 'hammedfola@gmail.com'
+    },
+    {
+        id: 7,
+        name: 'Williams Toritese',
+        username: 'toritese',
+        email: 'toritese@gmail.com'
+    },
+    {
+        id: 8,
+        name: 'Joy Akpan',
+        username: 'joy',
+        email: 'joy@gmail.com'
+    },
+    {
+        id: 9,
+        name: 'Jessica Okali',
+        username: 'jessica',
+        email: 'jessica@gmail.com'
+    },
+    {
+        id: 10,
+        name: 'Shola Fasasi',
+        username: 'shola',
+        email: 'shola@gmail.com'
+    }
+]
